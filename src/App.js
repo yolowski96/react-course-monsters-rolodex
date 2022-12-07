2d6dd6a830897a13e6315d60bd5f4f717dd81d7f
Goran Yolovski <goran@Gorans-MacBook-Pro.local>
2022-12-07 22:32:33 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import CardList from './components/card-list/card-list';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchingValue: ''
+    }
+  };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(
+        () => {
+          return { monsters: users }
+        },
+        () => { console.log(this.state) }
+      ))
+  }
+
+  onSearchChange = (event) => {
+    const searchingValue = event.target.value.toLowerCase();
+
+    this.setState(() => { return { searchingValue } })
+  };
+
+  render() {
+    const filterMonsters = this.state.monsters.filter((monster) => monster.name.toLowerCase().includes(this.state.searchingValue));
+    return (
+      <div className="App">
+        < CardList />
+        <input className='search-box' type='search' placeholder='search monsters' onChange={this.onSearchChange} />
+        {
+          filterMonsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/card-list/card-list.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class CardList extends Component {
+
+    render(){
+        return <div>HELLO FROM CARD COMP</div>
+    }
+}
+
+export default CardList
